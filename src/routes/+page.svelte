34f15d130a9c538637d28add0d5a44fd5640f114
sashajh2 <ajovanov@mit.edu>
2024-03-05 14:24:14 -0500

    fronnt photo added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 </svelte:head>
 <h1>Sasha Jovanovic-Hacon</h1>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio officia in explicabo velit unde ipsa vel quaerat voluptatum eligendi, consequuntur quibusdam perferendis recusandae harum, expedita, a quia aliquam maiores dolores.</p>
-<!-- <img src="../../static/images/IMG_4782.JPEG" alt="An image of the author in Bijeljina, Bosnia"> -->
+<img src="images/IMG_4782.JPEG" alt="An image of the author in Bijeljina, Bosnia" />
 <h2>Latest Projects</h2>
 <div 
     class="projects" 
